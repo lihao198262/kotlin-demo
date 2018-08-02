2a8b7d2f4b9bf207181284f615a560a43511688c
lihao8173 <lihao198262@163.com>
2018-08-02 14:02:57 +0800

    add thymeleaf html

--- src/main/kotlin/com/example/demo/KotlinDemoApplication.kt
+++ src/main/kotlin/com/example/demo/KotlinDemoApplication.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 
 @SpringBootApplication
 open class KotlinDemoApplication
@@ -11,9 +13,19 @@ open class KotlinDemoApplication
 @RestController
 class HelloController {
 	@GetMapping("/hello")
-	fun helloKotlin(): String {
+	fun helloKotlin(): String { 
 		return "hello world kotlin spring web"
 	}
+}
+
+@Controller
+class MainController {
+    @GetMapping("login")
+    fun login(modle : Model): String {
+        val a = 100;
+        modle.addAttribute("a", a); 
+        return "/login.html"
+    }
 }
 
 fun main(args: Array<String>) {
